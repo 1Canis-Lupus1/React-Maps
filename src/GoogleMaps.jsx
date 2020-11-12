9df12c2a2c786d3cce9177a5d0016c1a54418a5b
1Canis-Lupus1 <shubhamchoudhary081997@gmail.com>
2020-11-12 17:54:37 +0530

    User Input Added For google-maps-react

--- src/GoogleMaps.jsx
+++ src/GoogleMaps.jsx
@@ -2,17 +2,34 @@ import React, { Component } from "react";
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
 
 export class GoogleMaps extends Component {
+  state={
+    selectedPlace:{
+      name:"Ranchi"
+    }
+  }
+
+  handleChange=(e)=>{
+    console.log("Location:",e.target.value);
+    //To be sent as selectedPlace
+    this.setState({
+      selectedPlace:{
+        name:e.target.value
+      }
+    },()=>console.log("After Set State:",this.state.selectedPlace.name))
+  }
+
   render() {
     return (
       <div>
         <h2>My Map here</h2>
+        <input type="text" placeholder="Enter Location" onChange={this.handleChange}/>
         <Map google={this.props.google} zoom={14}>
-          <Marker onClick={this.onMarkerClick} name={"Current location"} />
+          <Marker onClick={(e)=>this.onMarkerClick(e)} name={"Current location"} />
 
           <InfoWindow onClose={this.onInfoWindowClose}>
-            {/* <div>
+             <div>
               <h1>{this.state.selectedPlace.name}</h1>
-            </div> */}
+            </div> 
           </InfoWindow>
         </Map>
       </div>
@@ -21,5 +38,5 @@ export class GoogleMaps extends Component {
 }
 
 export default GoogleApiWrapper({
-  apiKey: ("YOUR_GOOGLE_API_KEY_GOES_HERE")
+  apiKey: ("AIzaSyDQW8bd3zyLQgvyHIOBmaqhT9dfl7AQQ5U")
 })(GoogleMaps)
